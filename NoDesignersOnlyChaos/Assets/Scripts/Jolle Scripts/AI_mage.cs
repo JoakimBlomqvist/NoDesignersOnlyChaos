5fc7c718d7117642e4acf550a56a1326fef7b195
Johan_Blomqvist <johan.blomqvist@futuregames.nu>
2021-08-23 16:11:20 +0200

    NewRoom

---
+++ NoDesignersOnlyChaos/Assets/Scripts/Jolle Scripts/AI_mage.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_mage : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float waitTime;
+    public float StartWaitTime;
+
+    public Transform[] PatrolSpots;
+    private int randomSpot;
+
+    private void Start()
+    {
+        waitTime = StartWaitTime;
+        randomSpot = Random.Range(0, PatrolSpots.Length);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, PatrolSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, PatrolSpots[randomSpot].position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, PatrolSpots.Length);
+                waitTime = StartWaitTime;
+
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    
+
+
+}
