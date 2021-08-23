dfbb41ebef0b92d3acd508d9fd24e4e4c2a44a52
Joakim Blomqvist <joakim.blomqvist@futuregames.nu>
2021-08-23 21:37:21 +0200

    New SPELL

---
+++ NoDesignersOnlyChaos/Assets/SpermCharacter.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpermCharacter : MonoBehaviour
+{
+    public LayerMask targetLayer;
+    public float moveSpeed = 2f;    
+    private Collider2D Lockedtarget;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Lockedtarget == null)
+        {
+            Lockedtarget = Physics2D.OverlapCircle(transform.position, 8f, targetLayer);
+        }
+
+        if (Lockedtarget != null)
+        {
+            var direction = Vector2.zero;
+
+            direction = Lockedtarget.transform.position - transform.position;
+            rb.AddRelativeForce(direction.normalized * moveSpeed, ForceMode2D.Force);
+        }
+    }
+}
