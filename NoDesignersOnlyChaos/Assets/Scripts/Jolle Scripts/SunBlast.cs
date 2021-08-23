6aec9ee18b6c065e81c82c50ebcc943e903523c3
Johan_Blomqvist <johan.blomqvist@futuregames.nu>
2021-08-23 21:52:33 +0200

    New enemy added

---
+++ NoDesignersOnlyChaos/Assets/Scripts/Jolle Scripts/SunBlast.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunBlast : MonoBehaviour
+{
+    [SerializeField] private float speed_Sunblast = 150;
+    private Rigidbody2D rb;
+
+    
+    CharacterMovement characterMovement;
+    Vector2 moveDir;
+    Vector3 lastVelocity;
+    // Start is called before the first frame update
+    [SerializeField]
+    private void Awake()
+    {
+
+        characterMovement = GameObject.FindObjectOfType<CharacterMovement>();
+        rb = GetComponent<Rigidbody2D>();
+        if (characterMovement != null)
+        {
+            moveDir = (characterMovement.transform.position - transform.position).normalized * speed_Sunblast;
+            rb.AddForce(moveDir * speed_Sunblast);
+        }
+
+
+        Destroy(gameObject, 5f);
+
+    }
+
+    private void Update()
+    {
+        lastVelocity = rb.velocity;
+
+    }
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        
+        characterMovement = collider.collider.GetComponent<CharacterMovement>();
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            collider.gameObject.GetComponent<IDamageable>().TakeDamage(20);
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ NoDesignersOnlyChaos/Assets/Scripts/Jolle Scripts/Sunboss.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sunboss : MonoBehaviour
+{
+    [SerializeField] private TrailRenderer SunBlast;
+    [SerializeField] private float fireRate;
+    [SerializeField] float nextFire;
+    
+
+    void Awake()
+    {
+        
+        nextFire = Time.time;
+    }
+
+
+    void Update()
+    {
+
+            FireRate();
+
+    }
+
+    void FireRate()
+    {
+
+        if (Time.time > nextFire)
+        {
+            var sublast = Instantiate(SunBlast, transform.position, Quaternion.identity);
+            Physics2D.IgnoreCollision(sublast.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            nextFire = Time.time + fireRate;
+        }
+
+
+    }
+
+}
