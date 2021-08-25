6908be2f7f6a5c23a7160ac69008885729cef389
Johan_Blomqvist <johan.blomqvist@futuregames.nu>
2021-08-25 21:10:17 +0200

    update

--- NoDesignersOnlyChaos/Assets/Scripts/Health.cs
+++ NoDesignersOnlyChaos/Assets/Scripts/Health.cs
@@ -3,13 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour, IDamageable
 {
+    [SerializeField] public Image HealthBar;
+    public float CurrentHealth;
+    private float maxHealth;
     [SerializeField] private int health;
     [SerializeField] private ParticleSystem blood;
     public UnityEvent OnDamageTaken;
     public Action OnDeath;
+
+
+    private void Start()
+    {
+        maxHealth = health;
+        //HealthBar = GetComponent<Image>();
+    }
+
     public void TakeDamage(int damage)
     {
         if (OnDamageTaken != null)
@@ -21,6 +33,11 @@ public class Health : MonoBehaviour, IDamageable
         if (health <= 0)
         {
             Die();
+        }
+        if (HealthBar != null)
+        {
+            CurrentHealth = health;
+            HealthBar.fillAmount = CurrentHealth / maxHealth;
         }
     }
 
