3e8ec9f6887ab7741d613e61188682cdb139ecf3
Johan_Blomqvist <johan.blomqvist@futuregames.nu>
2021-08-26 18:08:16 +0200

    Some fixes

--- NoDesignersOnlyChaos/Assets/CleardRooms.cs
+++ NoDesignersOnlyChaos/Assets/CleardRooms.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class CleardRooms : MonoBehaviour
 {
     [SerializeField] private TMP_Text CleardRoomsText;
-    
+    private string rooms;
     // Start is called before the first frame update
     
     private void OnEnable()
