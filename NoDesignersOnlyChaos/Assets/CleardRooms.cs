f9bce7c02a2dd5434dc11fd3d4efed34c5807a87
Johan_Blomqvist <johan.blomqvist@futuregames.nu>
2021-08-26 17:57:50 +0200

    Update CleardRooms.cs

--- NoDesignersOnlyChaos/Assets/CleardRooms.cs
+++ NoDesignersOnlyChaos/Assets/CleardRooms.cs
@@ -8,14 +8,13 @@ using UnityEngine;
 public class CleardRooms : MonoBehaviour
 {
     [SerializeField] private TMP_Text CleardRoomsText;
-    RoomCurrency roomCurrency;
-    private string rooms;
+    
     // Start is called before the first frame update
     
     private void OnEnable()
     {
-        RoomCurrency.Instance.roomCounter.ToString(rooms);
-        CleardRoomsText.text = rooms;
+        CleardRoomsText.text = RoomCurrency.Instance.roomCounter.ToString();
+        
     }
 
 
