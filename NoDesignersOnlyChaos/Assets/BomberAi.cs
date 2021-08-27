05eaad9e1c7e535d0966d0a256a2e13b8eccb65f
Johan_Blomqvist <johan.blomqvist@futuregames.nu>
2021-08-27 17:58:16 +0200

    update bomberman

--- NoDesignersOnlyChaos/Assets/BomberAi.cs
+++ NoDesignersOnlyChaos/Assets/BomberAi.cs
@@ -16,6 +16,7 @@ public class BomberAi : MonoBehaviour
         Vector2 dir = (new Vector2(transform.position.x, transform.position.y)).normalized;
         rb.AddForce(dir * force);
         StartCoroutine(BombDropping());
+        //lastVelocity = rb.velocity;
     }
 
     private void Update()
@@ -25,19 +26,31 @@ public class BomberAi : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collider)
     {
+        //var player = collider.gameObject.CompareTag("player");
+        
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Physics2D.IgnoreCollision(collider.gameObject.GetComponent<Collider2D>() , gameObject.GetComponent<Collider2D>());
+            return;
+        }
         var speed = lastVelocity.magnitude;
         var direction = Vector3.Reflect(lastVelocity.normalized, collider.contacts[0].normal);
 
         
         rb.velocity = direction * Mathf.Max(speed, 0f);
+
+        
         StartCoroutine(BombDropping());
+
+        //StartCoroutine(BombDropping());
     }
     IEnumerator BombDropping()
     {
         yield return new WaitForSeconds(3f);
 
         var bomb = Instantiate(Bomb, gameObject.transform.position, Quaternion.identity);
-        Physics2D.IgnoreCollision(bomb.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(bomb.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
+        
         yield break;
     }
 }
