bedefe7c357f11a695a211e8ad510ecccad90a99
Gabinus <gabinus_93@hotmail.com>
2021-08-27 22:07:19 +0200

    so much confuse

--- NoDesignersOnlyChaos/Assets/Scripts/Utility/DealDamageOnContact.cs
+++ NoDesignersOnlyChaos/Assets/Scripts/Utility/DealDamageOnContact.cs
@@ -7,11 +7,16 @@ public class DealDamageOnContact : MonoBehaviour
 {
     [SerializeField] private string tagToDamage;
     [SerializeField] private int damage;
+    [SerializeField] private bool destroyOnContact;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag(tagToDamage))
         {
             other.gameObject.GetComponent<IDamageable>().TakeDamage(damage);
+            if (destroyOnContact)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
