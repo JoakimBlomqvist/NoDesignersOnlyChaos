0a8a9e5ed64c795d2996652c8459fa749ac8c836
Joakim Blomqvist <joakim.blomqvist@futuregames.nu>
2021-08-28 22:09:42 +0200

    l

--- NoDesignersOnlyChaos/Assets/Scripts/Spell/MagicHoming.cs
+++ NoDesignersOnlyChaos/Assets/Scripts/Spell/MagicHoming.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.Rendering.VirtualTexturing;
 
 public class MagicHoming : MonoBehaviour
 {
