ddf93dce31ae9ffef1988b69ffa76efaa3009b15
Joakim Blomqvist <joakim.blomqvist@futuregames.nu>
2021-09-03 14:49:47 +0200

    New blackhole

---
+++ NoDesignersOnlyChaos/Assets/Prefabs/AI/Stationary_AI.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stationary_AI : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+}
