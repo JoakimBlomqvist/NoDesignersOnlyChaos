fb4bef044ce20f62acb73bb3437f74aaa227f037
Joakim Blomqvist <joakim.blomqvist@futuregames.nu>
2021-09-06 17:21:57 +0200

    Ultis and stuff

--- NoDesignersOnlyChaos/Assets/UltimateManager.cs
+++ NoDesignersOnlyChaos/Assets/UltimateManager.cs
@@ -6,12 +6,43 @@ using UnityEngine;
 
 public class UltimateManager : MonoBehaviour
 {
-    //[SerializeField] private List<UltimateAbility> _ultimateAbilities;
+    [SerializeField] private List<UltimateAbility> _ultimateAbilities;
     [SerializeField] private UltimateAbility ultimateToUse;
 
     [ContextMenu("PickedUltimate")]
-    private void PickedUltimate()
+    private void OnEnable()
+    {
+        EventManager.instance.OnStartOfGame += SetUltimate;
+    }
+    private void OnDisable()
+    {
+        EventManager.instance.OnStartOfGame += SetUltimate;
+    }
+    
+    private void SetUltimate()
     {
         EventManager.instance.SetUltimate(ultimateToUse);
+    }
+
+    public void PickUltimate(int ult)
+    {
+        switch (ult)
+        {
+            case 0:
+                ultimateToUse = _ultimateAbilities[0];
+                break;
+            case 1:
+                ultimateToUse = _ultimateAbilities[1];
+                break;
+            case 2:
+                ultimateToUse = _ultimateAbilities[2];
+                break;
+            case 3:
+                ultimateToUse = _ultimateAbilities[3];
+                break;
+            case 4:
+                Debug.Log("Bool thing");
+                break;
+        }
     }
 }
