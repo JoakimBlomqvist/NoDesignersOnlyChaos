81609130b183104635e3b2d47092e08a997d2f7a
Gabinus <gabinus_93@hotmail.com>
2021-09-07 19:55:20 +0200

    as

---
+++ NoDesignersOnlyChaos/Assets/SuicideBomber.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SuicideBomber : Enemy
+{
+    //private RaycastHit2D hit;
+    [SerializeField] private float detectionRange;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private LayerMask playerLayer;
+    private Rigidbody2D rb;
+    [SerializeField] private float detectionFreq;
+    private bool playerInSight;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(LookForPlayerRoutine());
+    }
+
+    private void Update()
+    {
+        if (playerInSight)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void FindTarget()
+    {
+        Vector2 dirToPlayer = (PlayerManager.Instance.playerPosition - transform.position).normalized;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dirToPlayer, detectionRange);
+        Debug.DrawRay(transform.position, dirToPlayer, Color.red, 1f);
+        if (hit.collider.CompareTag("Player"))
+        {
+            playerInSight = true;
+            Debug.DrawRay(transform.position, dirToPlayer, Color.cyan, 1f);
+        }
+        else
+        {
+            playerInSight = false;
+        }
+    }
+    private void AttackTarget()
+    {
+        Vector2 direction = PlayerManager.Instance.playerPosition - transform.position;
+        rb.MovePosition(rb.position + direction.normalized * Time.deltaTime * moveSpeed);
+    }
+
+    IEnumerator LookForPlayerRoutine()
+    {
+        while (true)
+        {
+            FindTarget();
+            yield return new WaitForSeconds(detectionFreq);
+        }
+    }
+}
