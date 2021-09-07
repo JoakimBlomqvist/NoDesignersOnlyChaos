5fae44d0795c4b21d7a585bf2ac359452c4599c1
Joakim Blomqvist <joakim.blomqvist@futuregames.nu>
2021-09-07 17:39:12 +0200

    dash

--- NoDesignersOnlyChaos/Assets/Scripts/UnitScripts/Player/CharacterMovement.cs
+++ NoDesignersOnlyChaos/Assets/Scripts/UnitScripts/Player/CharacterMovement.cs
@@ -9,6 +9,11 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private GameObject DeathScreen;
     public float moveSpeed;
     [SerializeField] private Health hpScript;
+    private bool dashing;
+    private bool dashOnCd;
+    [SerializeField] private float dashStrength;
+    [SerializeField] private float dashDuration;
+    [SerializeField] private float dashCooldown;
     
     private Rigidbody2D rb;
 
@@ -29,13 +34,38 @@ public class CharacterMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        if (Input.GetMouseButtonDown(1) && !dashOnCd)
+        {
+            StartCoroutine(Dash());
+            StartCoroutine(DashCooldown());
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + new Vector2(horizontal, vertical)*Time.fixedDeltaTime*moveSpeed);
+        if (!dashing)
+        {
+            rb.MovePosition(rb.position + new Vector2(horizontal, vertical) * Time.fixedDeltaTime * moveSpeed);
+        }
     }
 
+    private IEnumerator Dash()
+    {
+        dashing = true;
+        Vector2 direction = (UtilsClass.GetMouseWorldPos() - new Vector2(transform.position.x, transform.position.y));
+        rb.velocity = Vector2.zero;
+        rb.velocity = direction.normalized * dashStrength;
+        yield return new WaitForSeconds(dashDuration);
+        rb.velocity = Vector2.zero;
+        dashing = false;
+    }
+
+    private IEnumerator DashCooldown()
+    {
+        dashOnCd = true;
+        yield return new WaitForSeconds(dashCooldown);
+        dashOnCd = false;
+    }
 
 
 }
